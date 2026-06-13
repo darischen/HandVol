419241fdb20f040af1b3a70c9cc2e2704375a47d
Daris Chen <daris.chen@gmail.com>
2026-06-13 04:36:39 -0700

    chore: remove out-of-scope pytest config from task 1

--- conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-# Hydra registers a broken pytest plugin that crashes on Python 3.14 due to a
-# dataclass change. Block it from loading before pytest tries to import it.
-sys.modules.setdefault("hydra", type(sys)("hydra"))
